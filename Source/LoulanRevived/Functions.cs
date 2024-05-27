b667cbf2a0513577f462457cd1972bd5e746263f
vivien <terametech@outlook.com>
2024-05-26 22:29:18 -0400

    add loulan revived mod

---
+++ Source/LoulanRevived/Functions.cs
@@ -0,0 +1,41 @@
+﻿using Assets.Scripts;
+using System.Collections;
+using System.IO;
+using System.Reflection;
+
+namespace LoulanRevived;
+
+internal class Functions
+{
+    public static void LoadIncidents(ref WorldManager worldManager)
+    {
+        string ModPath = Directory.GetParent(Assembly.GetExecutingAssembly().Location).FullName;
+
+        foreach (string directory in Directory.GetDirectories(Path.Combine(ModPath, "Incidents")))
+        {
+            MissionData incidentData = Incident.GetIncident(new DirectoryInfo(directory));
+
+            Incident.LoadIncident(incidentData);
+        }
+    }
+
+    public static IEnumerator DelayIncident(TileData tileData, Incident incident, int delay, WorldManager.TerrainFeatureIncident relatedValues)
+    {
+        yield return Yielders.WaitForSeconds(delay);
+        // code being removed:
+        //if (tileData.IsCenterTile)
+        //{
+        //    _ = Incident.LaunchIncident(incident);
+        //    if (relatedValues.IsRepeating)
+        //    {
+        //        TileSystem.Instance.GenerateRandomIncident(tileData, false);
+        //    }
+        //}
+        //else
+        if (relatedValues.CanLaunchOutsideTile)
+        {
+            _ = Incident.LaunchIncident(incident);
+        }
+        yield break;
+    }
+}
---
+++ Source/LoulanRevived/PatchFunctions.cs
@@ -0,0 +1,38 @@
+﻿#pragma warning disable CA1707
+
+using Assets.Scripts;
+using HarmonyLib;
+using JetBrains.Annotations;
+using System.Collections;
+
+namespace LoulanRevived;
+
+[HarmonyPatch]
+public static class PatchFunctions
+{
+    [UsedImplicitly]
+    [HarmonyPatch(typeof(WorldManager), "LoadGameDataAsync")]
+    [HarmonyPostfix]
+    public static void WorldManagerLoadGameDataAsync(ref WorldManager __instance)
+    {
+        if (Data.SpawnWrecks != null && Data.SpawnWrecks.Value)
+        {
+            Functions.LoadIncidents(ref __instance);
+        }
+    }
+
+    [UsedImplicitly]
+    [HarmonyPatch(typeof(TileSystem), "DelayIncident")]
+    [HarmonyPrefix]
+    public static bool TileSystemDelayIncident(ref TileSystem __instance, ref IEnumerator __result,
+        TileData tileData, Incident incident, int delay, WorldManager.TerrainFeatureIncident relatedValues)
+    {
+        if (Data.SpawnWrecks != null && Data.SpawnWrecks.Value)
+        {
+            __result = Functions.DelayIncident(tileData, incident, delay, relatedValues);
+            return false;
+        }
+
+        return true;
+    }
+}
