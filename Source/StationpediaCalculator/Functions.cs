585ef10c1fc20d9271ff73b9f86a33c663af8119
vivien <terametech@outlook.com>
2024-08-09 11:25:59 -0400

    add copy & paste capability

--- Source/StationpediaCalculator/Functions.cs
+++ Source/StationpediaCalculator/Functions.cs
@@ -1,28 +1,53 @@
-﻿using Assets.Scripts.UI;
+﻿using Assets.Scripts;
+using Assets.Scripts.UI;
+using Cysharp.Threading.Tasks;
 using NCalc;
-using UnityEngine;
+using System.Collections.Generic;
+using UnityObject = UnityEngine.Object;
 
 namespace StationpediaCalculator;
 
 public static class Functions {
-    public static void CalculateSearch(ref Stationpedia stationpedia, string inputText) {
+
+    public static void CalculateSearch(ref SPDAListItem calculatorItem, string inputText) {
         try {
-            var expression = new Expression(inputText, EvaluateOptions.IgnoreCase);
-            expression.Parameters["Pi"] = Mathf.PI;
+            if (Stationpedia.Instance != null && calculatorItem != null && !string.IsNullOrEmpty(inputText)) {
+                var expression = new Expression(inputText, EvaluateOptions.IgnoreCase);
 
-            if (expression.HasErrors()) {
-                Data.CalculatorItem.gameObject.SetActive(false);
-            }
-            else {
-                stationpedia.NoResultsFromSearchText.SetActive(false);
+                if (expression.HasErrors()) {
+                    calculatorItem.gameObject.SetActive(false);
+                }
+                else {
+                    var result = expression.Evaluate().ToString();
 
-                Data.CalculatorItem.InsertImage.sprite = stationpedia.ImportantSearchImage;
-                Data.CalculatorItem.SetSpecial();
-                Data.CalculatorItem.InsertTitle.text = expression.Evaluate().ToString();
-                Data.CalculatorItem.gameObject.SetActive(true);
-                Data.CalculatorItem.transform.SetSiblingIndex(0);
+                    calculatorItem.gameObject.SetActive(true);
+                    calculatorItem.transform.SetSiblingIndex(0);
+
+                    calculatorItem.InsertTitle.text = result;
+                    calculatorItem.InsertImage.sprite = Stationpedia.Instance.ImportantSearchImage;
+                    calculatorItem.SetSpecial();
+
+                    calculatorItem.InsertsButton.onClick.AddListener(async () => {
+                        Stationpedia.Instance.BaseAnimator.SetBool("Copied", true);
+                        GameManager.Clipboard = result;
+                        await UniTask.Delay(750);
+                        Stationpedia.Instance.ResetClipboardNotification();
+                    });
+
+                    Stationpedia.Instance.NoResultsFromSearchText.SetActive(false);
+                }
             }
         }
         catch { }
+    }
+
+    public static void CreateCalculator(ref List<SPDAListItem> items) {
+        if (Stationpedia.Instance != null && items != null) {
+            var calculatorItem = UnityObject.Instantiate(Stationpedia.Instance.ListInsertPrefab, Stationpedia.Instance.SearchContents);
+            calculatorItem.gameObject.SetActive(false);
+
+            Data.CalculatorItem = calculatorItem;
+            items.Add(calculatorItem);
+        }
     }
 }
--- Source/StationpediaCalculator/PatchFunctions.cs
+++ Source/StationpediaCalculator/PatchFunctions.cs
@@ -11,23 +11,17 @@ public static class PatchFunctions {
     [UsedImplicitly]
     [HarmonyPatch(typeof(Stationpedia), "ForceSearch")]
     [HarmonyPostfix]
-    public static void StationpediaForceSearch(ref Stationpedia __instance, string searchText) {
-        if (__instance != null && Data.CalculatorItem != null && !string.IsNullOrEmpty(searchText)) {
-            Functions.CalculateSearch(ref __instance, searchText);
-        }
+    public static void StationpediaForceSearchref(ref Stationpedia __instance, string searchText) {
+        Functions.CalculateSearch(ref Data.CalculatorItem, searchText);
     }
 
     [UsedImplicitly]
     [HarmonyPatch(typeof(Stationpedia), "AddSearchInsertsToPool")]
     [HarmonyPostfix]
     public static void StationpediaAddSearchInsertsToPool(ref Stationpedia __instance, int numToAdd) {
-        if (__instance != null) {
-            Data.CalculatorItem = UnityEngine.Object.Instantiate(__instance.ListInsertPrefab, __instance.SearchContents);
-            var traverse = Traverse.Create(__instance);
-            var list = traverse.Field("_SPDASearchInserts").GetValue<List<SPDAListItem>>();
-            list.Add(Data.CalculatorItem);
+        var traverse = Traverse.Create(Stationpedia.Instance);
+        var list = traverse.Field("_SPDASearchInserts").GetValue<List<SPDAListItem>>();
 
-            Data.CalculatorItem.gameObject.SetActive(false);
-        }
+        Functions.CreateCalculator(ref list);
     }
 }
