c0d7fe457ee8a0ca9f4c20b5ef06e71d23d210cc
vivien <terametech@outlook.com>
2025-01-09 18:17:40 -0500

    replace custom extensions with built-in harmony ones

--- Source/BetterCodeEditor/Functions.cs
+++ Source/BetterCodeEditor/Functions.cs
@@ -12,14 +12,10 @@ namespace BetterCodeEditor;
 internal static class Extensions {
     internal static int ToIndex(this int value, bool index) => index ? value - 1 : value;
 
-    internal static bool OpcodeIs(this CodeInstruction code, OpCode opcode) => code.opcode == opcode;
-
-    internal static bool InstructionIs(this CodeInstruction code, OpCode opcode, object operand) => code.OpcodeIs(opcode) && code.OperandIs(operand);
-
     internal static IEnumerable<CodeInstruction> ReplaceMaxLines(this IEnumerable<CodeInstruction> instructions, bool index = false) =>
         instructions.Manipulator(
             (instruction) => // if instruction opcode is Ldc_I4 AND the operand is MAX_LINES
-                instruction.InstructionIs(OpCodes.Ldc_I4, InputSourceCode.MAX_LINES.ToIndex(index)),
+                instruction.Is(OpCodes.Ldc_I4, InputSourceCode.MAX_LINES.ToIndex(index)),
             (instruction) => // change operand to our value or default if null
                 instruction.operand = Data.CodeEditorLines.ToIndex(index)
         );
@@ -27,7 +23,7 @@ internal static class Extensions {
     internal static IEnumerable<CodeInstruction> ReplaceMaxLineLength(this IEnumerable<CodeInstruction> instructions) =>
         instructions.Manipulator(
             (instruction) => // if instruction opcode is Ldc_I4 AND the operand is LINE_LENGTH_LIMIT
-                instruction.InstructionIs(OpCodes.Ldc_I4, InputSourceCode.LINE_LENGTH_LIMIT),
+                instruction.Is(OpCodes.Ldc_I4, InputSourceCode.LINE_LENGTH_LIMIT),
             (instruction) => // change operand to our value or default if null
                 instruction.operand = Data.CodeEditorLineLength
         );
@@ -35,7 +31,7 @@ internal static class Extensions {
     internal static IEnumerable<CodeInstruction> ReplaceMaxFileSize(this IEnumerable<CodeInstruction> instructions) =>
         instructions.Manipulator(
             (instruction) => // if instruction opcode is Ldc_I4 AND the operand is MAX_FILE_SIZE
-                instruction.InstructionIs(OpCodes.Ldc_I4, InputSourceCode.MAX_FILE_SIZE),
+                instruction.Is(OpCodes.Ldc_I4, InputSourceCode.MAX_FILE_SIZE),
             (instruction) => // change operand to our value or default if null
                 instruction.operand = Data.MaxFileSize
         );
