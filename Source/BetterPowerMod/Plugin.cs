42aabcae347b1458f3bce90c47279a9935b7c5ea
vivien <terametech@outlook.com>
2025-03-11 17:03:11 -0400

    change debug print

--- Source/BetterPowerMod/Plugin.cs
+++ Source/BetterPowerMod/Plugin.cs
@@ -110,7 +110,7 @@ public class Plugin : BaseUnityPlugin {
             }
             default:
             case Severity.Debug: {
-                Debug.Log(newMessage);
+                ConsoleWindow.Print(newMessage, color: ConsoleColor.Gray, aged: false);
             }
             break;
         }
--- Source/ColoredGases/Plugin.cs
+++ Source/ColoredGases/Plugin.cs
@@ -92,7 +92,7 @@ public class Plugin : BaseUnityPlugin {
             }
             default:
             case Severity.Debug: {
-                Debug.Log(newMessage);
+                ConsoleWindow.Print(newMessage, color: ConsoleColor.Gray, aged: false);
             }
             break;
         }
