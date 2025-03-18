e4ba366a0603d345c041fb2464ba37f56cc6498a
vivien <terametech@outlook.com>
2025-03-18 19:36:15 -0400

    undo finalizer

--- Source/BetterCodeEditor/PatchFunctions.cs
+++ Source/BetterCodeEditor/PatchFunctions.cs
@@ -9,16 +9,6 @@ public static class PatchFunctions {
     private static readonly Dictionary<MethodInfo, bool> _patches = typeof(PatchFunctions).GetMethods().ToDictionary(info => info, key => false);
 
     [UsedImplicitly]
-    [HarmonyPatch(typeof(PatchFunctions))]
-    [HarmonyFinalizer]
-    public static Exception Finalizer(Exception __exception) {
-        Plugin.LogException(__exception);
-
-        // suppress all patch exceptions
-        return null;
-    }
-
-    [UsedImplicitly]
     [HarmonyPatch(typeof(InputSourceCode), "HandleInput")]
     [HarmonyTranspiler]
     public static IEnumerable<CodeInstruction> InputSourceCodeHandleInputTranspiler(IEnumerable<CodeInstruction> instructions) {
