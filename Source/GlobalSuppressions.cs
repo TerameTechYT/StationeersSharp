6699234efcd92eed7868d7083bf9297250ccb683
vivien <terametech@outlook.com>
2025-03-18 16:57:31 -0400

    move global suppresions

---
+++ Source/GlobalSuppressions.cs
@@ -0,0 +1,27 @@
+﻿#region
+
+using System.Diagnostics.CodeAnalysis;
+
+#endregion
+
+[assembly: SuppressMessage("Performance", "CA1822:Mark members as static")]
+
+[assembly: SuppressMessage("Globalization", "CA1305:Specify IFormatProvider")]
+
+[assembly: SuppressMessage("Usage", "CA2243:Attribute string literals should parse correctly")]
+
+[assembly: SuppressMessage("Naming", "CA1720:Identifier contains type name")]
+[assembly: SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores")]
+[assembly: SuppressMessage("Naming", "CA1708:Identifiers should differ by more than case")]
+[assembly: SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores")]
+
+[assembly: SuppressMessage("Style", "IDE0009:Member access should be qualified.")]
+[assembly: SuppressMessage("Style", "IDE0060:Remove unused parameter")]
+[assembly: SuppressMessage("Style", "IDE0058:Expression value is never used")]
+
+[assembly: SuppressMessage("Design", "CA1998:Async method lacks 'await' operators and will run synchronously")]
+[assembly: SuppressMessage("Design", "CA1051:Do not declare visible instance fields")]
+[assembly: SuppressMessage("Design", "CA1034:Nested types should not be visible")]
+[assembly: SuppressMessage("Design", "CA1065:Do not raise exceptions in unexpected locations")]
+[assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types")]
+[assembly: SuppressMessage("Design", "CA1002:Do not expose generic lists")]
