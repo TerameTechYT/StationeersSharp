d02528e1db35ebe1ba857361412f6e42dd0681fe
vivien <terametech@outlook.com>
2025-07-13 13:26:13 -0400

    add IModInfo interface

---
+++ Source/StationeersLibrary/Mods/IModInfo.cs
@@ -0,0 +1,32 @@
+﻿#region
+
+#endregion
+
+namespace StationeersLibrary.Mods;
+
+public interface IModInfo {
+    // <summary>
+    /// Mod Name
+    /// </summary>
+    public string? Name { get; set; }
+
+    /// <summary>
+    /// Mod GUID
+    /// </summary>
+    public string? Guid { get; set; }
+
+    /// <summary>
+    /// Mod Version
+    /// </summary>
+    public Version? Version { get; set; }
+
+    /// <summary>
+    /// Mod Workshop Id
+    /// </summary>
+    public ulong WorkshopId { get; set; }
+
+    /// <summary>
+    /// Mod Game Type
+    /// </summary>
+    public GameType GameType { get; set; }
+}
