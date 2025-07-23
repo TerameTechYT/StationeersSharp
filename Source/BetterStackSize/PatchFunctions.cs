3444d3b7364fd2dd7fc1fea664358c0b89d61611
vivien <terametech@outlook.com>
2025-07-23 11:33:14 -0400

    remove Prefab.LoadAll patch

--- Source/BetterStackSize/PatchFunctions.cs
+++ Source/BetterStackSize/PatchFunctions.cs
@@ -6,15 +6,5 @@ namespace BetterStackSize;
 
 [HarmonyPatch]
 public static class PatchFunctions {
-    [HarmonyPatch(typeof(Prefab), nameof(Prefab.LoadAll)), HarmonyPriority(Priority.Last), HarmonyPrefix]
-    public static void PrefabLoadAll() {
-        try {
-            foreach (Thing prefab in WorldManager.Instance.SourcePrefabs) {
-                Plugin.Instance.ProcessThing(prefab);
-            }
-        }
-        catch (Exception ex) {
-            Utilities.ExceptionReporter(Plugin.Instance, ref ex);
-        }
-    }
+
 }
